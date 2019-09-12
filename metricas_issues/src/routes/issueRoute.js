4bdd966dc67e38ae2409f71c48accc1199aa9e28
pdaniel37 <pedrodaniel@candev.com.br>
2019-09-12 15:49:47 -0300

    feat(#31): Updating .gitignore and creating basic API

---
+++ metricas_issues/src/routes/issueRoute.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/issueController')
+
+router.post('/', controller.post)
+router.put('/:id', controller.put)
+router.delete('/:id', controller.delete)
+
+module.exports = router
