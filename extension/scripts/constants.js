9e77cea899a234d9fe350907fe067742f81af437
Pedro Daia Cardoso <pedro_daia@hotmail.com>
2019-09-16 12:12:42 -0300

    feat(#34): add lougout button and changed button styles

---
+++ extension/scripts/constants.js
@@ -0,0 +1,5 @@
+export default 
+{
+    CLIENT_ID: '75c7ceaa2fd180fdceb3',
+    CLIENT_SECRET: 'f70bab3c05d93de9a682de4e6874ca3a2ba680e0'
+}
---
+++ extension/scripts/popup.js
@@ -0,0 +1,39 @@
+import constants from './constants.js'
+
+const url = 
+`https://github.com/login/oauth/authorize?response_type=code&client_id=${constants.CLIENT_ID}&scope=repo`
+
+document.getElementById('loginButton').addEventListener("click", function() 
+{
+    window.open(url)
+}, false)
+
+document.getElementById('logoutButton').addEventListener("click", function()
+{
+    chrome.storage.sync.set({'oauth2_token': null}, function()
+    {
+        console.log("Token removido com sucesso")
+    })
+    window.close()
+})
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+    
+})
+
+chrome.storage.sync.get('oauth2_token', function(res) 
+{
+    if (res.oauth2_token != undefined)
+    {
+        console.log("Token salvo: " + res.oauth2_token)
+        let loginButton = document.getElementById('loginButton')
+        loginButton.parentNode.removeChild(loginButton)
+    }
+    else
+    {
+        console.log("Token nao disponivel")
+        let logoutButton = document.getElementById('logoutButton')
+        logoutButton.parentNode.removeChild(logoutButton)
+    }
+});
