6bb1e01a29c4b6e82e0e1989dcb06c0b6e0816c6
Pedro Daia Cardoso <pedro_daia@hotmail.com>
2019-09-22 17:38:08 -0300

    Revert "enabling github api requests"
    This reverts commit 383e9ed346560959339b9e8de1c194d69cad6bed.

--- APIs/branches_request/src/routes/branchRoute.js
+++ APIs/branches_request/src/routes/branchRoute.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/branchController')
 
-router.get('/', controller.get)
 router.post('/', controller.post)
 router.put('/:id', controller.put)
 router.delete('/:id', controller.delete)
