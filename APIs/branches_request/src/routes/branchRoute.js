383e9ed346560959339b9e8de1c194d69cad6bed
Pedro Daia Cardoso <pedro_daia@hotmail.com>
2019-09-22 17:32:56 -0300

    enabling github api requests

--- APIs/branches_request/src/routes/branchRoute.js
+++ APIs/branches_request/src/routes/branchRoute.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/branchController')
 
+router.get('/', controller.get)
 router.post('/', controller.post)
 router.put('/:id', controller.put)
 router.delete('/:id', controller.delete)
