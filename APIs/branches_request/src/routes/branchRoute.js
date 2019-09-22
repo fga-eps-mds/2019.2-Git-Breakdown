c3e8ab09b2209b7d71fd97c4df490e37efba9f7b
Pedro Daia Cardoso <pedro_daia@hotmail.com>
2019-09-22 17:09:24 -0300

    feat(#23): creating basic API structure

---
+++ APIs/branches_request/src/routes/branchRoute.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/branchController')
+
+router.post('/', controller.post)
+router.put('/:id', controller.put)
+router.delete('/:id', controller.delete)
+
+module.exports = router
