8c57847591ece8c340e7f5c0ac8403e24f2b7734
pdaniel37 <pedrodaniel@candev.com.br>
2019-09-25 01:55:49 -0300

    feat(#52): adding integration tests for issue microsservice
    Co-authored-by: jbisinotti <joao.bisinotti@candev.com.br>

--- APIs/api_gateway/test/index.js
+++ APIs/api_gateway/test/index.js
@@ -1,7 +1,49 @@
-const expect = require('chai').expect
-index = require('../../../index')
-request = require('supertest')(index)
+const chai = require('chai')
+const assert = chai.assert
+const request = require('request')
+const expect = chai.expect
+const index = require('../src/index').app
+const superRequest = require('supertest')
 
-describe('API Gateway tests', () => {
-    
+describe('API Gateway test', () => {
+    let urlBase = 'http://localhost:3000'
+    let urlParams = '?owner=fga-eps-mds&repository=2019.2-Git-Breakdown'
+    it('/issues: valid request', async () => {
+        await request.get( {
+            url : urlBase+'/issues'+urlParams
+        }, (error, response, body) => {
+                let _body = {}
+                try{
+                  _body = JSON.parse(body)
+                }
+                catch(e){
+                  _body = {}
+                }
+                console.log(body)
+                if(response.status != undefined)
+                    expect(response.status).to.equal(200)
+                expect(_body).to.have.property('open')
+                expect(_body).to.have.property('closed')
+                expect(_body).to.have.property('openPercent')
+                expect(_body).to.have.property('closedPercent')
+            }
+        )
+    })
+    it('/issues: invalid request', async () => {
+        await request.get( {
+            url : urlBase+'/issues'
+        }, (error, response, body) => {
+                let _body = {}
+                try{
+                  _body = JSON.parse(body)
+                }
+                catch(e){
+                  _body = {}
+                }
+                console.log(body)
+                if(response.statusCode != undefined)
+                    expect(response.statusCode).to.equal(400)
+            }
+        )
+    })
 });
