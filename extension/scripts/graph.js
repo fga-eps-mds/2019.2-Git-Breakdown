22e6cc90efececa3567f38d1873849f14cc08cf4
Pedro Daia Cardoso <pedro_daia@hotmail.com>
2019-09-25 14:15:35 -0300

    feat(#47):displaying basic pie chart

---
+++ extension/scripts/graph.js
@@ -0,0 +1,46 @@
+const url_base = 'http://54.159.51.170:3000/issues'
+const url_aux = '?owner=fga-eps-mds&repository=2019.2-Git-Breakdown'
+
+const ctx = document.getElementById('issueStatusChart').getContext('2d')
+const issueChart = new Chart(ctx, 
+{
+    type: 'pie',
+    data: 
+    {
+        labels: ['open', 'closed'],
+        datasets: 
+        [{
+            label: '# of issues',
+            data: [2, 3],
+            backgroundColor: 
+            [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: 
+            [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: 
+    {
+        title:
+        {
+            display: true,
+            text: 'Issues state chart'
+        },
+        scales: 
+        {
+            yAxes: 
+            [{
+                ticks: 
+                {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
