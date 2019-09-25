692a80d5347414969c5f78fe6eb818114ebd2140
pdaniel37 <pedrodaniel@candev.com.br>
2019-09-25 04:26:11 -0300

    feat(#52): adding integration tests for branch microsservice
    Co-authored-by: jbisinotti <joao.bisinotti@candev.com.br>

--- APIs/api_gateway/test/index.js
+++ APIs/api_gateway/test/index.js
@@ -2,10 +2,8 @@ const chai = require('chai')
 const assert = chai.assert
 const request = require('request')
 const expect = chai.expect
-const index = require('../src/index').app
-const superRequest = require('supertest')
 
-describe('API Gateway test', () => {
+describe('API Gateway integration tests', () => {
     let urlBase = 'http://localhost:3000'
     let urlParams = '?owner=fga-eps-mds&repository=2019.2-Git-Breakdown'
     
@@ -100,6 +98,39 @@ describe('API Gateway test', () => {
     it('/pullrequests: invalid request', async () => {
         await request.get( {
             url : urlBase+'/pullrequests'
+        }, (error, response, body) => {
+                let _body = {}
+                try{
+                  _body = JSON.parse(body)
+                }
+                catch(e){
+                  _body = {}
+                }
+                if(response.statusCode != undefined)
+                    expect(response.statusCode).to.equal(400)
+            }
+        )
+    })
+    
+    it('/branches: valid request', async () => {
+        await request.get( {
+            url : urlBase+'/branches'+urlParams
+        }, (error, response, body) => {
+                let _body = {}
+                try{
+                  _body = JSON.parse(body)
+                }
+                catch(e){
+                  _body = {}
+                }
+                if(response.status != undefined)
+                    expect(response.status).to.equal(200)
+            }
+        )
+    })
+    it('/branches: invalid request', async () => {
+        await request.get( {
+            url : urlBase+'/branches'
         }, (error, response, body) => {
                 let _body = {}
                 try{
