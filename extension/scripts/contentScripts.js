b3b4b9c64c63c8ad0144527f7a1a06960cdc2c0f
wdvictor <wd.victorhugo@gmail.com>
2019-09-26 20:11:00 -0300

    style(#48):working with the css to adjust the button tab Co-authored-by: paulohgontijoo <paulohgontijoo@gmail.com>

--- extension/scripts/contentScripts.js
+++ extension/scripts/contentScripts.js
@@ -7,9 +7,23 @@ const breakdownTAB = () =>{
 
     const content = document.getElementsByClassName('hx_reponav reponav js-repo-nav js-sidenav-container-pjax container zh-attached')
     var tab = document.createElement('div')
-    var tab_link = document.createElement('a')
-    tab_link.innerHTML = 'Gitbreakdown'
-    tab.appendChild(tab_link)
+    tab.style.textAlign = "center"
+    tab.style.paddingLeft = "12px"
+    tab.style.paddingRight = "12px"
+    tab.style.paddingTop = "7px"
+    tab.style.paddingBottom = "8px"
+    tab.style.borderTop = "3px"
+    tab.style.borderLeft = "1px"
+    tab.style.borderRight = "1px"
+    tab.style.borderBottom = "1px"
+
+    tab.innerHTML =  `
+        <a href="#breakdown" style="text-align:center;">
+            <span class="span" style="text-align:center;">
+                BreakDown
+            </span>
+        </a>
+    `
     content[0].appendChild(tab)
 
 }
