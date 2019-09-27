5cf1a208b0cbf1a971f38236916ecc76709aaae2
wdvictor <wd.victorhugo@gmail.com>
2019-09-27 19:57:04 -0300

    fix(#48):Implementing if statement that corrects a break in the app Co-authored-by: paulohgontijoo <paulohgontijoo@gmail.com>

--- extension/scripts/contentScripts.js
+++ extension/scripts/contentScripts.js
@@ -6,30 +6,28 @@
 const breakdownTAB = () =>{
 
     const content = document.getElementsByClassName('hx_reponav reponav js-repo-nav js-sidenav-container-pjax container zh-attached')
-    var tab = document.createElement('div')
-    tab.style.textAlign = "center"
-    tab.style.paddingLeft = "12px"
-    tab.style.paddingRight = "12px"
-    tab.style.paddingTop = "7px"
-    tab.style.paddingBottom = "8px"
-    tab.style.borderTop = "3px"
-    tab.style.borderLeft = "1px"
-    tab.style.borderRight = "1px"
-    tab.style.borderBottom = "1px"
-
-
-
-
-
-    tab.innerHTML =  `
-        <a href="#breakdown" style=" color: #586069; text-decoration: none; ">
-            <span class="span" style="text-align:center;">
-                BreakDown
-            </span>
-        </a>
-    `
-    content[0].appendChild(tab)
-
+    if(content.length != 0)
+    {
+        var tab = document.createElement('div')
+        tab.style.textAlign = "center"
+        tab.style.paddingLeft = "12px"
+        tab.style.paddingRight = "12px"
+        tab.style.paddingTop = "7px"
+        tab.style.paddingBottom = "8px"
+        tab.style.borderTop = "3px"
+        tab.style.borderLeft = "1px"
+        tab.style.borderRight = "1px"
+        tab.style.borderBottom = "1px"
+        tab.innerHTML =  `
+            <a href="#breakdown" style=" color: #586069; text-decoration: none; ">
+                <span class="span" style="text-align:center;">
+                    BreakDown
+                </span>
+            </a>
+        `
+        content[0].appendChild(tab)
+    }
+   
 }
 
 breakdownTAB()
