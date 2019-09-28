52329480bbbe1a4ee072fdce1d162a6ea36d64e9
jbisinotti <joao.bisinotti@candev.com.br>
2019-09-27 21:19:02 -0300

    fix(#21): rearranging files and fixing errors

--- APIs/commit_request/server/server.js
+++ APIs/commit_request/server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const routes = require('../routes/commitRoute')
+const routes = require('../routes/commitsRoute')
 
 
 app.use('/', routes)
