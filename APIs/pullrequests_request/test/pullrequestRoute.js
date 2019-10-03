32cbfbd38c555bec3101597de8c0b172c438cd75
jbisinotti <joao.bisinotti@candev.com.br>
2019-10-03 17:52:07 -0300

    feat(#60): adding unit tests
    Co-authored-by: pxpc2 <pedro_daia@hotmail.com>

--- APIs/pullrequests_request/test/pullrequestRoute.js
+++ APIs/pullrequests_request/test/pullrequestRoute.js
@@ -3,53 +3,56 @@ const issues = require('../src/routes/pullrequestRoute')
 const assert = chai.assert
 const request = require('request')
 const should = require('should')
+const axios = require('axios')
 const expect = chai.expect
 chai.use(require('chai-json'));
 
 const urlBase = 'http://localhost:3000/pullrequests'
 
 describe('PullRequests route tests', () => {
-  it('Test: Request valid', (done) => {
-    request.get(
+  it('Test: Request valid', () => {
+    axios.get(
       {
         url : urlBase + '?owner=fga-eps-mds&repository=2019.2-Git-Breakdown'
       },
-      (error, response, body) => {
+      async (error, response, body) => {
 
         let _body = {};
         try{
-          _body = JSON.parse(body);
+          _body = await JSON.parse(body);
         }
         catch(e){
           _body = {};
         }
-          
-        /*for(issue in body){
-           expect(body).to.have.nested.property(issue.pull_request)
-        }*/
-          expect(response.statusCode).to.equal(200)
-        done();
+
+        if(response.status != undefined)
+            expect(response.statusCode).to.equal(200);
+
+        if(_body != undefined){
+            expect(_body).to.have.property('open')
+            expect(_body).to.have.property('closed')
+            expect(_body).to.have.property('refused_percent')
+        }
       }
     );
   });
 
-  it('Test: Request without parameters', (done) => {
-    request.get(
+  it('Test: Request without parameters', () => {
+    axios.get(
       {
         url : urlBase + '?owner=f'
       },
-      (error, response, body) => {
+      async (error, response, body) => {
 
         let _body = {};
         try{
-          _body = JSON.parse(body);
+          _body = await JSON.parse(body);
         }
         catch(e){
           _body = {};
         }
 
         expect(response.statusCode).to.equal(400);
-        done();
       }
     );
   });
