bd7fe01c7ce4c7de870e3e8bdb8acaf4bc2ea87f
Mateus Oliveira Patrício <o.mateus.p@gmail.com>
2019-10-07 22:46:50 -0300

    Revert "Feat#71/gitlabcideploy"

--- deploy/disableHostKeyChecking.sh
+++
@@ -1,4 +0,0 @@
-set -e
-mkdir -p ~/.ssh
-touch ~/.ssh/config
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
--- extension/scripts/constants.js
+++ extension/scripts/constants.js
@@ -1,7 +1,5 @@
 export default 
 {
     CLIENT_ID: '75c7ceaa2fd180fdceb3',
-    CLIENT_SECRET: 'f70bab3c05d93de9a682de4e6874ca3a2ba680e0',
-    OWNER_KEY: '',
-    REPO_KEY: ''
+    CLIENT_SECRET: 'f70bab3c05d93de9a682de4e6874ca3a2ba680e0'
 }
