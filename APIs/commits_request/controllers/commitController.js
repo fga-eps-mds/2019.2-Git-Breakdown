4b78ba077f2bccf7522b5b9f1371120b95386061
PAULO HENRIQUE COSTA GONTIJO <paulohgontijoo@gmail.com>
2019-10-10 16:15:58 -0300

    feat(#63): adjusting await functions
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>
    Co-authored-by: wdvictor <victorhpantunes@gmail.com>

--- APIs/commits_request/controllers/commitController.js
+++ APIs/commits_request/controllers/commitController.js
@@ -1,5 +1,5 @@
-const request = require('request')
 const express = require('express')
+const axios = require('axios')
 const commit_route = express.Router()
 
 commit_route.get = async (req, res, next) => {
@@ -18,58 +18,45 @@ commit_route.get = async (req, res, next) => {
         const gitApiUrl = 'https://api.github.com'
         const url_endpoint = `${gitApiUrl}/repos/${owner}/${repository}/${endpoint}`
         const header_option = {
-            url: url_endpoint,
-            method: 'GET',
             headers: {
                 'Accept': 'application/json',
                 'Accept-Charset': 'utf-8',
                 'User-Agent': '2019.2-Git-Breakdown',
+                'Authorization': `token ${req.query.token}`
             }
         }
 
         try {
-            await request.get(header_option, async (err, response, body) => {
-                if (err)
-                    throw err
-                else {
-                    //commits hold a vector of json's data
-                    const commits = JSON.parse(body)
-                    await commits.forEach((commit, index, array) => {
-                        request.get({
-                            headers: {
-                                'Accept': 'application/json',
-                                'Accept-Charset': 'utf-8',
-                                'User-Agent': '2019.2-Git-Breakdown',
-                                'Authorization': `token ${req.query.token}`
-                            },
-                            uri: commit.url
-                        }, function (error, response, body) {
-                            if (error)
-                                throw error
-                            else {
-                                let singleCommit = JSON.parse(body)
-                                let match = false
-                                for (let contributor in contributorsInformation) {
-                                    if (contributorsInformation[contributor].name === singleCommit.author.login) {
-                                        contributorsInformation[contributor].commits += 1
-                                        contributorsInformation[contributor].additions += singleCommit.stats.additions
-                                        contributorsInformation[contributor].deletions += singleCommit.stats.deletions
-                                        match = true
-                                        contributorsInformation[0].commits += 1
-                                    }
-                                }
-                                if (match === false) {
-                                    let committer = { 'name': singleCommit.author.login, 'commits': 1, 'additions': singleCommit.stats.additions, 'deletions': singleCommit.stats.deletions }
-                                    contributorsInformation.push(committer)
-                                    contributorsInformation[0].commits += 1
-                                }
-                                if (contributorsInformation[0].commits === array.length){
-                                    return res.status(200).json(contributorsInformation)
-                                }
+            await axios.get(url_endpoint, header_option).then(async (response) => {
+                //commits hold a vector of json's data
+                const commits = response.data
+                await commits.forEach((commit, index, array) => {
+                    axios.get(commit.url, header_option).then(function (response) {
+                        let singleCommit = response.data
+                        let match = false
+                        for (let contributor in contributorsInformation) {
+                            if (contributorsInformation[contributor].name === singleCommit.author.login) {
+                                contributorsInformation[contributor].commits += 1
+                                contributorsInformation[contributor].additions += singleCommit.stats.additions
+                                contributorsInformation[contributor].deletions += singleCommit.stats.deletions
+                                match = true
+                                contributorsInformation[0].commits += 1
                             }
-                        })
+                        }
+                        if (match === false) {
+                            let committer = { 'name': singleCommit.author.login, 'commits': 1, 'additions': singleCommit.stats.additions, 'deletions': singleCommit.stats.deletions }
+                            contributorsInformation.push(committer)
+                            contributorsInformation[0].commits += 1
+                        }
+                        if (contributorsInformation[0].commits === array.length) {
+                            return res.status(200).json(contributorsInformation)
+                        }
+                    }).catch(err => {
+                        console.log(err)
                     })
-                }
+                })
+            }).catch(function (err) {
+                console.log(err)
             })
         } catch (err) {
             return res.status(500).json(err)
@@ -77,5 +64,5 @@ commit_route.get = async (req, res, next) => {
     }
 }
 
-//export this functionality has a module
+//export this functionality as a module
 module.exports = commit_route
