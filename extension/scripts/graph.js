67b125eceba8a5776bcb10da84809893562d08ab
Pedro Daia Cardoso <pedro_daia@hotmail.com>
2019-10-18 01:18:23 -0300

    feat(#62): changing dashboard to bar chart

--- extension/scripts/graph.js
+++ extension/scripts/graph.js
@@ -20,13 +20,13 @@ export function createCommitsChart(url_aux, repo_name)
         const ctx = document.getElementById('commitsDashboard').getContext('2d')
         const commitsChart = new Chart(ctx, 
         {
-            type: 'pie',
+            type: 'bar',
             data: 
             {
                 labels: names,
                 datasets: 
                 [{
-                    label: '# of branches',
+                    label: '# of commits per contributor',
                     data: qtCommits,
                     backgroundColor: colorArray,
                     borderColor: colorArray,
@@ -230,11 +230,11 @@ function getRandomColorArray(arraySize)
     let colorArray = []
     for (let i = 0; i < arraySize; i++)
     {
-        const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+        const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
         const randomByte = () => randomNumber(0, 255)
         const randomPercent = () => (randomNumber(50, 100) * 0.01).toFixed(2)
         const randomCssRgba = `rgba(${[randomByte(), randomByte(), randomByte(), randomPercent()].join(', ')})`
-        colorArray[i] = randomCssRgba
+            colorArray[i] = randomCssRgba
     }
     return colorArray
 }
