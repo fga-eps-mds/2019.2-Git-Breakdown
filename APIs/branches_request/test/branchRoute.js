67b1c1465796de261d9745510a8690d04211b878
PAULO HENRIQUE COSTA GONTIJO <paulohgontijoo@gmail.com>
2019-10-18 18:22:02 -0300

    feat(#78): refactoring branch testing
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>

--- APIs/branches_request/test/branchRoute.js
+++ APIs/branches_request/test/branchRoute.js
@@ -1,56 +1,52 @@
 const chai = require('chai')
 const branches = require('../src/routes/branchRoute')
 const assert = chai.assert
-const request = require('request')
 const axios = require('axios')
 const expect = chai.expect
-
-const urlBase = 'http://localhost:3000/branches'
+const urlBase = 'http://localhost:3004/branches'
+const urlEndpoint = urlBase + '?owner=fga-eps-mds&repository=2019.2-Git-Breakdown'
+const url = urlBase + '?owner=f'
 
 describe('Branches route tests', () => {
-  it('Test: Request valid', () => {
-    axios.get(
-      {
-        url : urlBase + '?owner=fga-eps-mds&repository=2019.2-Git-Breakdown'
-      },
-      (error, response, body) => {
+  it('Test: Request valid', (done) => {
+    axios.get(urlEndpoint).then(response => {
 
-        let _body = {};
+        let _body = {}
         try{
-          _body = JSON.parse(body);
+          _body = response.data
         }
         catch(e){
-          _body = {};
+          _body = {}
         }
 
         if(response.status != undefined)
-            expect(response.statusCode).to.equal(200);
+            expect(response.status).to.equal(200);
 
         if(_body != undefined){
             expect(_body).to.have.property('active_branches')
             expect(_body).to.have.property('percentage_merged')
         }
       }
-    );
-  });
+    ).catch(err => {
+      console.log(err)
+    })
+    done()
+  })
 
-  it('Test: Request without parameters', () => {
-    axios.get(
-      {
-        url : urlBase + '?owner=f'
-      },
-      async (error, response, body) => {
+  it('Test: Request without parameters', (done) => {
+    axios.get(url).then(response => {
 
-        let _body = {};
+        let _body = {}
         try{
-          _body = await JSON.parse(body);
+          _body = response.data
         }
         catch(e){
-          _body = {};
+          _body = {}
         }
-
-        expect(response.statusCode).to.equal(400);
       }
-    );
-  });
-});
+    ).catch(err => {
+      expect(err.response.status).to.equal(400)
+    })
+    done()
+  })
+})
