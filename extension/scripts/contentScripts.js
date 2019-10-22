a382fe51aab56744934a598d15e2792e50a1a883
wdvictor <wd.victorhugo@gmail.com>
2019-10-22 12:58:56 -0300

    feat(#98):fixing contentScript load function
    Co-authored-by: pxpc2 <pedro_daia@hotmail.com>

--- extension/scripts/contentScripts.js
+++ extension/scripts/contentScripts.js
@@ -1,13 +1,8 @@
-
 const load = (file) => {
-    console.log(`:SYS: load(${file})`)
     var src = document.createElement('script')
-    src.setAttribute('type', 'Module')
+    src.setAttribute('type', 'module')
     src.setAttribute('src', file)
     document.getElementsByTagName('head')[0].appendChild(src)
 }
 
-//The first routine necessary to start the app: load the button
 load('./button.js')
-
-
