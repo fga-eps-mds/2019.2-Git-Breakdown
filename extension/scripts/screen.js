74a5cafec59dff56f27d2864e3040923e9007c2b
wdvictor <wd.victorhugo@gmail.com>
2019-10-23 11:47:45 -0300

    react(#98):adding screen.js module
    Co-authored-by: pxpc2 <pedro_daia@hotmail.com>

---
+++ extension/scripts/screen.js
@@ -0,0 +1,96 @@
+const gbdScreen = () => 
+{
+    console.log("7")
+    const addCss = () => 
+    {
+        let innerStyle = 
+        `   
+        * {
+            margin:0px;
+            padding:0px;
+            font-family:sans-serif
+          }
+          
+          #gbd-screen {
+            border : 0.5px solid rgba(39, 31, 31, 0.5);
+            position : relative;
+            width : 1000px;
+            height : 500px;
+            
+          }
+          
+          #gbd-sidebar {
+            position: absolute;
+            display: block;
+            text-decoration: none;
+            width : 30px;
+            height : 100%;
+            background:#151719;
+            -webkit-transition :width 0.5s;
+            transition : width 0.5s;
+          }
+          
+          #gbd-sidebar:hover {
+            width : 200px;
+          }
+          
+          #gbd-sidebar a{
+            text-decoration : none;
+            display : block;
+            list-style-type : none;
+            color : rgba(230, 230, 230);
+            text-align : center;
+            border-bottom: 1px solid rgba(100, 100, 100,0.3);
+            margin : 40px;
+            overflow:hidden;
+          }
+
+          #gbd-sidebar p{
+            color : rgba(230, 230, 230);
+            overflow: hidden;
+            margin : 45px;
+          }
+
+        `
+        let css = document.createElement('style')
+        css.innerHTML = innerStyle
+        let head =  document.getElementsByTagName('head')
+        head[0].appendChild(css)  
+    }
+
+    const innerScreen = 
+        `    
+        <div id="gbd-screen">
+            <div id="gbd-sidebar">
+                <p>GitBreakDown</p>
+                <a href="#">home</a>
+                <a href="#">About us</a>
+                <a href="#">About the metris </a>
+            </div>
+        </div>
+        `
+
+    const repoContent = document.getElementsByClassName('repository-content')
+    const screen = document.createElement('div')
+    screen.innerHTML = innerScreen
+    if(document.getElementsByClassName('gbd-screen').length == 0)
+    {
+        repoContent[0].parentNode.insertBefore(screen, repoContent[0])
+        repoContent[0].parentNode.removeChild(repoContent[0])
+        addCss()
+    }
+
+    
+}
+
+// event()
+const GbdButtonOnClick = () => 
+{
+    console.log("6-GdbButtonOnClick()")
+    const gbdtab = document.getElementById('gbd-button')
+    gbdtab.addEventListener('click', gbdScreen)
+}
+
+
+
+GbdButtonOnClick()
