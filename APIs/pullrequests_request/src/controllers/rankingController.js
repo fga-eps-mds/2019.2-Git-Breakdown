4f155690a9fa3a83490b8d5b36d5593a273671c6
PAULO HENRIQUE COSTA GONTIJO <paulohgontijoo@gmail.com>
2019-10-24 20:19:39 -0300

    feat(#101): filtering merged pull requests
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>
    Co-authored-by: jbisinotti <joao.bisinotti@candev.com.br>

--- APIs/pullrequests_request/src/controllers/rankingController.js
+++ APIs/pullrequests_request/src/controllers/rankingController.js
@@ -7,6 +7,7 @@ exports.get = async (req, res, next) => {
     const repository = req.query.repository
     const token = req.query.token
     const endpoint = 'pulls'
+    contributorsInformation = [{ 'pullrequests': 0 }]
     
     if(owner === undefined || repository === undefined || token === undefined){
         res.status(400).send('Error 400: Bad Request')
@@ -24,10 +25,38 @@ exports.get = async (req, res, next) => {
             params: queryString
         }
         
-        await axios.get(url_endpoint, header_option).then( response => {
+        await axios.get(url_endpoint, header_option).then(async response => {
             let pullrequests = response.data
             
-            res.status(200).json(pullrequests)
+            function filterPullRequests(pullrequest){
+                if(pullrequest.state === 'closed' && pullrequest.merged_at != undefined){
+                    return true
+                }
+                return false
+            }
+            let filteredPullRequests = pullrequests.filter(filterPullRequests)
+            
+            await filteredPullRequests.forEach((pr, index, array) => {
+
+                let match = false
+                for (let contributor in contributorsInformation) {
+                    if (contributorsInformation[contributor].name === pr.user.login) {
+                        contributorsInformation[contributor].merged_pull_requests += 1
+                        match = true
+                    }
+                }
+                    if (match === false) {
+                        let committer = { 'name': pr.user.login,
+                                          'merged_pull_requests': 1, 
+                                          'comments': 0 }
+                        contributorsInformation.push(committer)
+                    }
+                    contributorsInformation[0].pullrequests += 1
+
+                    if (contributorsInformation[0].pullrequests === array.length) {
+                        return res.status(200).json(contributorsInformation)
+                    }
+                })
         }).catch(function (err) {
                 console.log(err)
         })
