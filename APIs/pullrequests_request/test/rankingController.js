2b9a66f6eb006c0abeb65c95e24b30253cb0e370
jbisinotti <joao.bisinotti@candev.com.br>
2019-10-25 18:21:00 -0300

    feat(#101): finishing pullrequests ranking test
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>
    Co-authored-by: paulohgontijoo <paulohgontijoo@gmail.com>

--- APIs/pullrequests_request/test/rankingController.js
+++ APIs/pullrequests_request/test/rankingController.js
@@ -33,4 +33,21 @@ describe('PullRequests ranking test', () => {
     })
     done()
   })
+
+  it('Test: Request without parameters', (done) => {
+    axios.get(urlBase).then(response => {
+
+        let _body = {};
+        try{
+          _body = response.data
+        }
+        catch(e){
+          _body = {}
+        }
+      }
+    ).catch(err => {
+      expect(err.response.status).to.equal(400)
+    })
+    done()
+  })
 })
