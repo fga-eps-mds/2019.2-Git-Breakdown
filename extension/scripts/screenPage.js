bf96fc34cab3acb71c556983bd3b6300649e0d6a
Pedro Daia Cardoso <pedro_daia@hotmail.com>
2019-11-07 14:07:38 -0300

    feat(#119): adding bootstrap css and changing navbar style
    Co-authored-by: wdvictor <victorhpantunes@gmail.com>

--- extension/scripts/screenPage.js
+++ extension/scripts/screenPage.js
@@ -1,21 +1,41 @@
 function gbdScreen()
 {
+    let url = chrome.extension.getURL("images/logo.jpg")
     let gbdScreen = 
     `
     <div class="container-fluid">    
     <div id="gbdScreen">
-        <div id="gbdNavBar">
-            <a id="gbdHomeBtn" href="#breakdown">Home</a>
-            <div class="subNav">
-                <button class="subNavBtn">Metrics</button>
-                <div class="subNavContent">
-                    <a href="#breakdown/commits">Commits</a>
-                    <a href="#breakdown/issues">Issues</a>
-                    <a href="#breakdown/branches">Branches</a>
-                    <a href="#breakdown/pr">Pull Request</a>
-                </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="box-shadow: 1px 1px 1px 1px black;
+        border-radius: 20px;">
+
+            <a class="navbar-brand" href="#breakdown"><img src="${url}" width="30" height="30" class="d-inline-block align-top"> GitBreakdown</a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a id="gbdHomeBtn" class="nav-link" href="#breakdown">Home</a>
+                </li>
+
+                <li class="nav-item dropdown active">
+                    
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Metrics
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#breakdown/commits">Commits</a>
+                        <a class="dropdown-item" href="#breakdown/issues">Issues</a>
+                        <a class="dropdown-item" href="#breakdown/branches">Branches</a>
+                        <a class="dropdown-item" href="#breakdown/pr">Pull Request</a>
+                    </div>
+
+                </li>
+
+                </ul>
+
             </div>
-        </div>
+        </nav>
+
         <div class="gbdContent">
             <div class="flexContainer">
                 <canvas id="commitsDashboard"></canvas>
