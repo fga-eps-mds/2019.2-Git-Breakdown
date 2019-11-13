639d979eca9010b872f9aea632e36c4627972808
wdvictor <wd.victorhugo@gmail.com>
2019-11-12 23:00:25 -0300

    refact(#143):refacting the html indentetion
    Co-authored-by: pxpc2 <pedro_daia@hotmail.com>
    Co-authored-by: paulohgontijoo <paulohgontijoo@gmail.com>

--- extension/scripts/screenPage.js
+++ extension/scripts/screenPage.js
@@ -5,54 +5,43 @@ function gbdScreen()
     let gbdScreen = 
     `
     <div class="container-fluid">    
-    <div id="gbdScreen">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="box-shadow: 1px 1px 1px 1px black;
-        border-radius: 20px;">
-
-            <a class="navbar-brand" href="#breakdown"><img src="${urlLogo}" width="30" height="30" class="d-inline-block align-top"> GitBreakdown</a>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a id="gbdHomeBtn" class="nav-link" href="#breakdown">Home</a>
-                </li>
-
-                <li class="nav-item dropdown active">
-                    
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Metrics
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#breakdown/commits">Commits</a>
-                        <a class="dropdown-item" href="#breakdown/issues">Issues</a>
-                        <a class="dropdown-item" href="#breakdown/branches">Branches</a>
-                        <a class="dropdown-item" href="#breakdown/pr">Pull Request</a>
-                    </div>
-
-                </li>
-
-                </ul>
-
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <button type="button" class="btn btn-outline-info" id="settingsButton" data-toggle="modal" data-target="#settingsModal">
-                            <img src="${urlCog}" width="30" height="30" class="d-inline-block align-top">
-                        </button>
-                    </li>
-                </ul>
-
-                <div class="modal fade" id="settingsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-
-                            <div class="modal-header">
-                                <h2 class="modal-title">Settings</h2>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
+        <div id="gbdScreen">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="box-shadow: 1px 1px 1px 1px black;
+            border-radius: 20px;">
+                <a class="navbar-brand" href="#breakdown"><img src="${urlLogo}" width="30" height="30" class="d-inline-block align-top"> GitBreakdown</a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a id="gbdHomeBtn" class="nav-link" href="#breakdown">Home</a>
+                            </li>
+                            <li class="nav-item dropdown active">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Metrics
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#breakdown/commits">Commits</a>
+                                    <a class="dropdown-item" href="#breakdown/issues">Issues</a>
+                                    <a class="dropdown-item" href="#breakdown/branches">Branches</a>
+                                    <a class="dropdown-item" href="#breakdown/pr">Pull Request</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <button type="button" class="btn btn-outline-info" id="settingsButton" data-toggle="modal" data-target="#settingsModal">
+                                    <img src="${urlCog}" width="30" height="30" class="d-inline-block align-top">
                                 </button>
-                            </div>
-
+                            </li>
+                        </ul>
+                        <div class="modal fade" id="settingsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h2 class="modal-title">Settings</h2>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
                             <div class="modal-body">
                                 <form>
                                     <div id="formInput">
@@ -61,82 +50,70 @@ function gbdScreen()
                                     </div>
                                 </form>
                             </div>
-
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                 <button type="button" class="btn btn-primary" id="settingsSave">Save</button>
                             </div>
-
                         </div>
                     </div>
                 </div>
-
             </div>
         </nav>
-
         <div class="gbdContent">
-        <div class="row">
-
-
-
-        <div class="col">
-        <table class="table table-striped table-dark ranking">
-            <thead>
-                <tr>
-                <th scope="col">Rank</th>
-                <th scope="col">User</th>
-                <th scope="col">Score</th>
-                </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>pxpc2</td>
-                        <td>2000</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>wdvictor</td>
-                        <td>420</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>baea</td>
-                        <td>120</td>
-                    </tr>
-                    </tbody>
-                </table>
-                </div>
-
-        <div class="col">
             <div class="row">
-                <div class="card text-white bg-dark mb-3">
-                    <canvas id="issuesDashboard"></canvas>   
+                <div class="col">
+                    <table class="table table-striped table-dark ranking">
+                        <thead>
+                            <tr>
+                                <th scope="col">Rank</th>
+                                <th scope="col">User</th>
+                                <th scope="col">Score</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>pxpc2</td>
+                                <td>2000</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>wdvictor</td>
+                                <td>420</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>baea</td>
+                                <td>120</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col">
+                    <div class="row">
+                        <div class="card text-white bg-dark mb-3">
+                            <canvas id="issuesDashboard"></canvas>   
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="card text-white bg-dark mb-3">
+                            <canvas id="branchesDashboard"></canvas> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="row">
+                        <div class="card text-white bg-dark mb-3 right">
+                            <canvas id="commitsDashboard"></canvas>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="card text-white bg-dark mb-3 right">
+                            <canvas id="prsDashboard"></canvas>   
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="card text-white bg-dark mb-3">
-                    <canvas id="branchesDashboard"></canvas> 
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="row">
-
-                <div class="card text-white bg-dark mb-3 right">
-                    <canvas id="commitsDashboard"></canvas>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="card text-white bg-dark mb-3 right">
-                    <canvas id="prsDashboard"></canvas>   
-                </div>
-            </div>
-        </div>
-        </div>
         </div>
     </div>
     </div>
