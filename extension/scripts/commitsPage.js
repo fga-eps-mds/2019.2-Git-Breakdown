bd552197c86da8629ab51ea5370f849388e2aaec
Pedro Daia Cardoso <pedro_daia@hotmail.com>
2019-11-22 15:06:28 -0300

    feat(#146): adding table header and cards to commits page
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>
    Co-authored-by: paulohgontijoo <paulohgontijoo@gmail.com>

--- extension/scripts/commitsPage.js
+++ extension/scripts/commitsPage.js
@@ -1,16 +1,37 @@
-
 function commitsPage(){
 
     let commitPage = 
     `
-        <h2 class="metricsPageTitle">Commits Status</h2>
-        <div id="metricContainer" class="container-fluid">
-            <div id="repoRanking" class="overflow-auto list-group">
-            </div>
-            <div id="progressContainer" class="container-fluid">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="table-responsive">
+                    <table class="table table-hover table-dark" id="commitsRanking">
+                        <thead>
+                            <tr>
+                                <th scope="col">Rank</th>
+                                <th scope="col">User</th>
+                                <th scope="col">Total of commits</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    </div>
+                </div>
 
+                <div class="col">
+                    <div class="row">
+                        <div class="card text-white bg-dark mb-3">
+                            <canvas id="commitsTimeDashboard"></canvas>   
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="card text-white bg-dark mb-3">
+                            <canvas id="commitsTimeDashboard"></canvas>   
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </div> 
     `
     return commitPage
 }
