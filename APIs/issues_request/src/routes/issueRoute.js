4b9a128ae0492132a2d5f3496753c85d06c0a587
jbisinotti <joao.bisinotti@candev.com.br>
2019-11-27 15:40:31 -0300

    bugFix(#175): editing issues microsservice
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>

--- APIs/issues_request/src/routes/issueRoute.js
+++ APIs/issues_request/src/routes/issueRoute.js
@@ -4,9 +4,6 @@ const issueController = require('../controllers/issueController')
 const rankingController = require('../controllers/rankingController')
 
 router.get('/', issueController.get)
-router.post('/', issueController.post)
-router.put('/:id', issueController.put)
-router.delete('/:id', issueController.delete)
 router.get('/ranking', rankingController.get)
 
 module.exports = router
