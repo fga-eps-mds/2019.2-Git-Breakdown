1db27543db07c4e09df4f98ef308a560ace58830
pdaniel37 <pedrodaniel@candev.com.br>
2019-11-27 15:52:51 -0300

    feat(#112): improving coverage on issue rankingController test
    Co-authored-by: jbisinotti <victormax2006@gmail.com>

--- APIs/issues_request/test/rankingController.js
+++ APIs/issues_request/test/rankingController.js
@@ -1,50 +1,67 @@
-const chai = require('chai')
 const axios = require('axios')
-const expect = chai.expect
+const request = require('supertest')
+const app = require('../src/app')
 
-const urlBase = 'http://localhost:3002/routes/ranking'
-const token = require('../../constants')
-const urlEndpoint = urlBase + '?owner=fga-eps-mds&repository=2019.2-Git-Breakdown=' + token
+const token = require('../../constants').token
 
 describe('Issues ranking tests', () => {
-  it('Test: Request valid', (done) => {
-    axios.get(urlEndpoint).then(response => {
 
-        let _body = {}
-        try{
-          _body = response.data
-        }
-        catch(e){
-          _body = {}
-        }
-
-        expect(response.status).to.equal(200);
-        body.should.have.property('issues');
-      }
-    ).catch(err => {
-      const errorResponse = err
+    it('should respond with status code 200', async() => {
+        const res = await request(app).get(`/issues/ranking?owner=fga-eps-mds&repository=2019.2-Git-Breakdown&token=${token}`)
+        
+        expect(res.body).toEqual(expect.any(Array));
+        expect(res.body[0]).toHaveProperty('name', 'opened_issues', 'closed_issues', 'comments')
+        expect(res.statusCode).toEqual(200);
     })
-    done()
-  })
-
-  it('Test: Request without parameters', (done) => {
-    axios.get(urlBase).then(response => {
-
-        let _body = {};
-        try{
-          _body = response.data
-        }
-        catch(e){
-          _body = {}
-        }
-      }
-    ).catch(err => {
-      if(err.response.status === 400){
-        expect(err.response.status).to.equal(400)
-      }else{
-        expect(err.response.status).to.equal(404)
-      } 
+    
+    it('should respond with status code 400', async() => {
+        const res = await request(app).get(`/issues/ranking?owner=fga-eps-mds&repository=2019.2-Git-Breakdown`)
+        
+        expect(res).toEqual(expect.any(Object));
+        expect(res.statusCode).toEqual(400);
     })
-    done()
-  }) 
-})  
+
+})
+
+//describe('Issues ranking tests', () => {
+//  it('Test: Request valid', (done) => {
+//    axios.get(urlEndpoint).then(response => {
+//
+//        let _body = {}
+//        try{
+//          _body = response.data
+//        }
+//        catch(e){
+//          _body = {}
+//        }
+//
+//        expect(response.status).to.equal(200);
+//        body.should.have.property('issues');
+//      }
+//    ).catch(err => {
+//      const errorResponse = err
+//    })
+//    done()
+//  })
+//
+//  it('Test: Request without parameters', (done) => {
+//    axios.get(urlBase).then(response => {
+//
+//        let _body = {};
+//        try{
+//          _body = response.data
+//        }
+//        catch(e){
+//          _body = {}
+//        }
+//      }
+//    ).catch(err => {
+//      if(err.response.status === 400){
+//        expect(err.response.status).to.equal(400)
+//      }else{
+//        expect(err.response.status).to.equal(404)
+//      } 
+//    })
+//    done()
+//  }) 
+//})  
