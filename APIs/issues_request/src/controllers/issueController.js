2a83044b1eeda86b5cf1b3c152d2b07368c6138b
jbisinotti <joao.bisinotti@candev.com.br>
2019-11-26 23:54:58 -0300

    bugFix(#175): extracting functions on issues controller
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>

--- APIs/issues_request/src/controllers/issueController.js
+++ APIs/issues_request/src/controllers/issueController.js
@@ -1,6 +1,44 @@
 const axios = require('axios')
 
-const queryString = { state:'all', per_page: 10000 }
+const queryString = { state:'all', per_page: 100 }
+
+async function requisition (link, token){
+    let issuesJson = {}
+    const header_option = {
+        headers: {
+            'Accept': 'application/vnd.github.v3+json',
+            'Accept-Charset': 'utf-8',
+            'User-Agent': '2019.2-Git-Breakdown',
+            'Authorization': `token ${token}`
+        },
+        params: queryString
+    } 
+
+    await axios.get(link, header_option).then( response => {
+        let issues = response.data
+        let link = response.headers.link.split(',')
+
+        return issues
+    }).then(issues => {
+        let filteredIssues = issues.filter(filterIssues)
+        let openIssues = filteredIssues.filter(getOpenIssues)
+        let closedIssues = filteredIssues.filter(getClosedIssues)
+        let tIssues = Object.keys(filteredIssues).length //number of all issues
+
+        let oIssues = Object.keys(openIssues).length //number of open issues
+        let cIssues = Object.keys(closedIssues).length //number of closed issues
+        let percentOfOpen = parseFloat(((oIssues/tIssues)*100).toFixed(2))
+        let percentOfClosed = parseFloat(((cIssues/tIssues)*100).toFixed(2))
+
+        let issuesInformation = {'open': oIssues, 'closed': cIssues, 'openPercent': percentOfOpen, 'closedPercent': percentOfClosed}
+        
+        issuesJson = issuesInformation
+        return issuesJson
+    }).catch(function (err) {
+        console.log(err)
+    })
+    return issuesJson
+}
 
 exports.get = async (req, res, next) => {
     const owner = req.query.owner
@@ -14,44 +52,9 @@ exports.get = async (req, res, next) => {
         const gitApiUrl = 'https://api.github.com'
         const url_endpoint = `${gitApiUrl}/repos/${owner}/${repository}/${endpoint}`
         
-    if(req.query.date != undefined){    
-        let date = new Date()
-        let selectedDate = req.query.date.split('-') //date first format: DD-MM-YYYY
-        let day = selectedDate[0]
-        let month = selectedDate[1]
-        let year = selectedDate[2]
-        let hour = 0
-        date.setFullYear(year, month -1, day)
-        date.setHours(hour - 3, 0, 0, 0)
-        queryString.since = date
-    }
-    
-    const header_option = {
-            headers: {
-                'Accept': 'application/vnd.github.v3+json',
-                'Accept-Charset': 'utf-8',
-                'User-Agent': '2019.2-Git-Breakdown',
-                'Authorization': `token ${token}`
-            },
-            params: queryString
-        }    
-
-    await axios.get(url_endpoint, header_option).then( response => {
-            let issues = response.data
-            let filteredIssues = issues.filter(filterIssues)
-            let openIssues = filteredIssues.filter(getOpenIssues)
-            let closedIssues = filteredIssues.filter(getClosedIssues)
-            let tIssues = Object.keys(filteredIssues).length //number of all issues
-            let oIssues = Object.keys(openIssues).length //number of open issues
-            let cIssues = Object.keys(closedIssues).length //number of closed issues
-            let percentOfOpen = parseFloat(((oIssues/tIssues)*100).toFixed(2))
-            let percentOfClosed = parseFloat(((cIssues/tIssues)*100).toFixed(2))
-
-            let issuesInformation = {'open': oIssues, 'closed': cIssues, 'openPercent': percentOfOpen, 'closedPercent': percentOfClosed}
-            res.status(200).json(issuesInformation)
-        }).catch(function (err) {
-                console.log(err)
-        })
+        let json = await requisition(url_endpoint, token)
+        
+        res.status(200).json(json)
     }
 }
 
@@ -68,6 +71,8 @@ exports.delete = (req, res, next) => {
     let id = req.params.id
     res.status(200).send(`Requisição recebida com sucesso! ${id}`)
 }
+
+
 
 function filterIssues(issue) {
     if(issue.pull_request === undefined){
