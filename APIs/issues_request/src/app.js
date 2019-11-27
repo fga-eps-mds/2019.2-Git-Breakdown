97b1b0c7c95527dc4fb3ad383c8d705c19d55988
pdaniel37 <pedrodaniel@candev.com.br>
2019-11-27 15:48:53 -0300

    bugFix(#175): removing deprecated files
    Co-authored-by: jbisinotti <victormax2006@gmail.com>

--- APIs/issues_request/src/app.js
+++ APIs/issues_request/src/app.js
@@ -2,10 +2,8 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 //Rotas
-const index = require('./routes/index')
 const issueRoute = require('./routes/issueRoute')
 
-app.use('/', index)
 app.use('/issues', issueRoute)
 
 module.exports = app
